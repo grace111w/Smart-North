23ba04ea50661ee90924501307e4471f0d483bfe
illingworthava <illingworth.ava@icloud.com>
2023-03-05 15:57:23 -0600

    added space

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>sign in </Text>
+      <Text> sign in </Text>
       <StatusBar style="auto" />
     </View>
   );
