b1af03eee17ce2955c57957d67a311cfdace9c53
illingworthava <illingworth.ava@icloud.com>
2023-03-12 17:03:48 -0500

    hii

--- App.js
+++ App.js
@@ -3,16 +3,14 @@ import { View, Text, TextInput, } from 'react-native'
 
 const LoginScreen = () => {
   return (
-    <><View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Login Screen</Text>
-    </View>
-    
-    <View>
-      <TextInput placeholder='Email'/>
+  <><View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+  <Text>Login Screen</Text>
+</View>
 
-    </View></>
+<View>
+  <TextInput placeholder='Email'/>
 
-  );
-};
-
+</View></>
+  )
+  }
 export default LoginScreen
