3cc4ff1749692b9a664a366ede7cab3b3bab5850
illingworthava <illingworth.ava@icloud.com>
2023-03-15 20:35:54 -0500

    login button

--- App.js
+++ App.js
@@ -5,11 +5,12 @@ const LoginScreen = () => {
   return ( 
     <><>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Login Screen</Text>
+        <Text>Login Screen!</Text>
         </View>
 
 </><View style={{ flex: 2, justifyContent: 'center', alignContent: 'center'}}>
         <TextInput placeholder='Email' />
+        <button title='Login'/>
       </View></>
   )
 };
