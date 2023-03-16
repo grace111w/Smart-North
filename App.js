c40d05d907c12615f364fa23246a7ab4a606c341
illingworthava <illingworth.ava@icloud.com>
2023-03-15 19:55:31 -0500

    test

--- App.js
+++ App.js
@@ -2,16 +2,17 @@ import React from 'react'
 import { View, Text, TextInput, SafeAreaView} from 'react-native'
 
 const LoginScreen = () => {
-  return (
+  return ( 
+  
   <><View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
   <Text>Login Screen</Text>
 </View>
 
 <View>
-  <SafeAreaView>
+  
   <TextInput placeholder='Email'/>
 </View>
-</SafeAreaView>
-  )
-  }
+ 
+  );
+};
 export default LoginScreen
