5e3e477f50c3fca05c7652b61d72b2752c33b401
grace111w <ligra000@isd284.com>
2023-03-18 18:55:54 -0500

    text inputs yayyy

--- App.js
+++ App.js
@@ -1,29 +1,37 @@
-import React from 'react'
-import { StyleSheet, View, Text, TextInput, Button} from 'react-native'
+import React from "react";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 
 const LoginScreen = () => {
-  return ( 
-    <View><>
-      <View style={styles.appPage}>
-        <TextInput placeholder='Email'/>
-        <TextInput placeholder='Password'/>
+  return (
+    <View>
+      <>
+        <View style={styles.appPage}>
+          <TextInput
+            style={{
+              height: 40,
+              borderColor: "gray",
+              borderWidth: 2,
+            }}
+            placeholder="Email"
+          />
+          <TextInput placeholder="Password" />
         </View>
-
-     </><View>
-       <Button title= "Login"/>
-       <Button title= "Sigh Up"/>
-      </View></View>
-  )
+      </>
+      <View>
+        <Button title="Login" />
+        <Button title="Sign Up" />
+      </View>
+    </View>
+  );
 };
-
-
 const styles = StyleSheet.create({
   appPage: {
     flex: 1,
-    padding: 50, 
+    padding: 50,
     backgroundColor: "#c3dbfa",
     alignItems: "stretch",
     justifyContent: "center",
+    color: "black",
   },
 });
-export default LoginScreen
+export default LoginScreen;
