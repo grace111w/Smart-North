4a8bfe813ed3eeb93190661a35ce09a9f17e7b07
grace111w <ligra000@isd284.com>
2023-03-19 15:09:41 -0500

    yayyyyyy no errors

--- coursepage.js
+++ coursepage.js
@@ -1,2 +1,8 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
+export default function coursepage() {
+  <View>
+    <Text>hi i hate this</Text>
+  </View>;
+}
