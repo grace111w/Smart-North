f2fe6c9e86c47aa0ee171a6f99d006f3ac2cacdb
mcnanava <mcnanava@icloud.com>
2023-03-18 19:42:40 -0500

    istg

--- coursepage.js
+++ coursepage.js
@@ -3,7 +3,7 @@ import {StyleSheet, Text, View} from 'react-native';
 
 const App = () => (
     <View style={styles.container}>
-      <Text style={styles.title}>React Native</Text>
+      <Text style={styles.title}>Courses</Text>
     </View>
   );
 
