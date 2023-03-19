7fd56c996defa2205b8cd9546e545a3faa55ed76
illingworthava <illingworth.ava@icloud.com>
2023-03-19 14:53:52 -0500

    jhguy

--- homepage.js
+++ homepage.js
@@ -57,7 +57,7 @@ const style = StyleSheet.create({
   appPage: {
     backgroundColor: "#c3dbfa",
     alignItems: "stretch",
-    justifyContent: "flex-end",
+    justifyContent: "space-evenly",
   },
 
 
