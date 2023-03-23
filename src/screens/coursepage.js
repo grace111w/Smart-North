0d76c9aa90848f7f3284834eaf8b57f94256b0ad
mcnanava <mcnanava@icloud.com>
2023-03-22 22:50:56 -0500

    peepee

--- src/screens/coursepage.js
+++ src/screens/coursepage.js
@@ -1,23 +1,61 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Button} from "react-native";
 
-const coursepage = (props) => {
+export default function App(){
   return (
-    <SafeAreaView>
-      <View style={style.appPage}>
+    <View style={styles.container}>
+      <Text>Courses</Text>
+    </View>
+  );
+}
+
+export default function App () {
+  return (
+    <View style={styles.container}>
+      <SafeAreaView>
         <View>
           <Button
             onPress={() => props.navigation.navigate("coursepage")}
             title="Minneapolis"
-          />
-        </View>
-        <View>
+            style={styles.blueButton}
+            />
           <Button
             onPress={() => props.navigation.navigate("coursepagedeerriver")}
             title="Deer River"
+            style={styles.orangeButton}
           />
         </View>
-      </View>
     </SafeAreaView>
+</View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#C3DBFA',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  blueButton: {
+    margin: 10,
+    borderWidth: 2,
+    tintColor: '#475467',
+    padding: 10,
+    textDecorationColor: '#FFFFFF',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  orangeButton: {
+      margin: 10,
+      borderWidth: 2,
+      tintColor: '#F38120',
+      padding: 10,
+      textDecorationColor: '#FFFFFF',
+  },
+  coursebuttonrow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
