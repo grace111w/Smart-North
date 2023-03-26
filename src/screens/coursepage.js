ea39498bd4831b67a06ef8f6acb135e16ae12f09
illingworthava <illingworth.ava@icloud.com>
2023-03-26 12:47:35 -0500

    jfgj

--- src/screens/coursepage.js
+++ src/screens/coursepage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, View, Button } from "react-native";
 
 // export default function App(){
 //   return (
