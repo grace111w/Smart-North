13d34c966cde120290a0100c57b08f527ee93571
illingworthava <illingworth.ava@icloud.com>
2023-04-11 22:16:18 -0500

    ;ljer

--- src/screens/coursepagedeerriver.js
+++ src/screens/coursepagedeerriver.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import { StyleSheet, View, Button } from "react-native";
+import { StyleSheet, View, Button, Text } from "react-native";
 
-export default function coursepage() {
+export default function (props) {
   return (
     <View style={styles.container}>
       <SafeAreaView>
+      <Text style={styles.coursestext}>Deer River Courses</Text>
         <View>
           <Button
             onPress={() => props.navigation.navigate("coursepage")}
             title="Minneapolis"
-            style={styles.blueButton}
+            style={styles.orangeButton}
           />
         </View>
       </SafeAreaView>
