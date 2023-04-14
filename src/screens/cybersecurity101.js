2e81c18f72c64e96602e33873bd762722f9878a6
illingworthava <illingworth.ava@icloud.com>
2023-04-13 21:53:16 -0500

    k4ewi35

--- src/screens/cybersecurity101.js
+++ src/screens/cybersecurity101.js
@@ -1,3 +1,21 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
 import { StyleSheet, View, Button, Text } from "react-native";
+
+export default function (props) {
+    return (
+      <View style={styles.container}>
+        <SafeAreaView>
+        <Text style={styles.coursestext}>Deer River Courses</Text>
+          <View>
+            <Button
+              onPress={() => props.navigation.navigate("coursepage")}
+              title="Minneapolis"
+              style={styles.orangeButton}
+            />
+          </View>
+        </SafeAreaView>
+      </View>
+    );
+  }
+  
