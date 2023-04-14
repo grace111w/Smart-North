509d912c922c38db96b6ba71d638bcbcd9a458a6
grace111w <ligra000@isd284.com>
2023-04-13 21:53:40 -0500

    yayyy fixed error

---
+++ src/screens/cybersecurity101.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function cybersecurity101() {
+  return (
+    <View>
+      <Text>cybersecurity101</Text>
+    </View>
+  )
+}
