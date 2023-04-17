5938a16f6217ce886598a3fd849f0ed0dc3229b4
illingworthava <illingworth.ava@icloud.com>
2023-04-16 23:55:06 -0500

    spodkf

--- src/screens/signupcyber.js
+++ src/screens/signupcyber.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { SafeAreaView, ScrollView } from "react-native";
-import { StyleSheet, Pressable, Text, Image, View } from "react-native";
+import { StyleSheet, Pressable, Text, Input, View } from "react-native";
 
 export default function (props) {
     return (
