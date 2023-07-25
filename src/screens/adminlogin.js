fa7b2450b596e874e4286ea7b99e462b3b8c0807
illingworthava <illingworth.ava@icloud.com>
2023-07-25 13:19:31 -0500

    j[oewfk[owegf]]

--- src/screens/adminlogin.js
+++ src/screens/adminlogin.js
@@ -1,127 +1,118 @@
-import React from "react";
 import {
-  StyleSheet,
-  View,
-  TextInput,
-  style,
-  Button,
-  SafeAreaView,
-  Pressable,
-} from "react-native";
-
-export default function (props) {
-  return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.space}>
-        <Pressable
-          style={styles.orangeButton}
-          onPress={() => props.navigation.navigate("generalinfo")}
-        ></Pressable>
-        <Pressable
-          style={styles.orangeButton}
-          onPress={() => props.navigation.navigate("userinfo")}
-        ></Pressable>
-      </View>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f5a449",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  pic: {
-    flex: 1,
-    width: 100,
-    height: null,
-    resizeMode: "contain",
-    marginTop: -390,
-    marginBottom: -500,
-  },
-  blueButton: {
-    margin: 10,
-    borderWidth: 2,
-    padding: 10,
-    textDecorationColor: "#FFFFFF",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    borderRadius: 50,
-    alignItems: "center",
-    paddingVertical: 15,
-    paddingHorizontal: 2,
-    elevation: 3,
-    backgroundColor: "#475467",
-  },
-  orangeButton: {
-    borderWidth: 1,
-    borderColor: "#475467",
-    justifyContent: "center",
-    marginTop: -40,
-    marginBottom: -50,
-    alignItems: "center",
-    backgroundColor: "#68a4ff",
-  },
-  space: {
-    padding: 50,
-  },
-  coursestext: {
-    fontWeight: "bold",
-    fontSize: 30,
-    color: "#475467",
-    justifyContent: "center",
-    flex: 1,
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  row: {
-    width: 50,
-    flex: 1,
-  },
-  course: {
-    borderColor: "#f38120",
-    borderWidth: 10,
-    borderRadius: 10,
-    backgroundColor: "#f38120",
-    flex: 1,
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  row: {
-    width: "50%",
-    flex: 1,
-  },
-  courselabel: {
-    fontSize: 25,
-    Color: "#475467",
-  },
-  coursetext: {
-    fontSize: 15,
-    fontweight: "bold",
-    Color: "#475467",
-    marginBottom: 20,
-  },
-  learnmore: {
-    borderWidth: 1,
-    borderColor: "#475467",
-    flex: 1,
-    justifyContent: "center",
-    height: 35,
-    width: 100,
-    marginLeft: 150,
-    marginBottom: 10,
-    borderRadius: 50,
-    backgroundColor: "#f38120",
-    alignItems: "center",
-  },
-  whitetext2: {
-    fontSize: 15,
-    color: "white",
-  },
-  whitetext: {
-    fontSize: 25,
-    color: "white",
-  },
-});
+    View,
+    Text,
+    SafeAreaView,
+    StyleSheet,
+    Pressable,
+    ScrollView,
+    Image,
+  } from "react-native";
+  import React from "react";
+  import Input from "../component/Input";
+  import { auth } from "../../FireBaseconfig";
+  import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+  import smart from "../img/IMG_1272.jpg";
+  import { useFonts } from "expo-font";
+  const Loginpage = (props) => {
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [user, setUser] = React.useState(null);
+    const handleLogin = () => {
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userCredentials) => {
+          const user = userCredentials.user;
+          setUser(user);
+          console.log(user);
+        })
+        .catch((error) => alert(error.message));
+    };
+  
+   
+  
+    React.useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setUser(user);
+          props.navigation.navigate("coursepage");
+        }
+      });
+      return unsubscribe;
+    }, []);
+  
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <Image style={styles.pic} source={smart}></Image>
+          <Input
+            label="Email address"
+            iconName="envelope-square"
+            placeholder="Email"
+            onChangeText={(text) => setEmail(text)}
+          />
+          <View />
+          <Input
+            label="Password"
+            iconName="key"
+            placeholder="Password"
+            password
+            onChangeText={(text) => setPassword(text)}
+          />
+          <View style={styles.scrollContainer}>
+            <Pressable style={styles.button} onPress={handleLogin}>
+              <Text style={styles.textButton}>Login</Text>
+            </Pressable>
+          </View>
+         
+        </ScrollView>
+      </SafeAreaView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#C3DBFA",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    pic: {
+      flex: 1,
+      width: 375,
+      resizeMode: "contain",
+      marginTop: -500,
+      marginBottom: -500,
+    },
+    scrollContainer: {
+      padding: 20,
+      paddingHorizontal: 20,
+    },
+    textFitle: {
+      fontSize: 30,
+      fontFamily: "Barlow",
+      fontWeight: "bold",
+      color: "#475467",
+    },
+    button: {
+      borderWidth: 0.5,
+      borderRadius: 100,
+      alignItems: "center",
+      paddingVertical: 22,
+      paddingHorizontal: 5,
+      justifyContent: "center",
+      borderRadius: 100,
+      backgroundColor: "#68a4ff",
+    },
+    textButton: {
+      textAlign: "center",
+      fontSize: 16,
+      color: "#475467",
+      fontWeight: "bold",
+    },
+    text: {
+      textAlign: "center",
+      fontSize: 16,
+      color: "#475467",
+      fontWeight: "bold",
+    },
+  });
+  export default Loginpage;
